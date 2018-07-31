302fa5c4b911686cbf14a6b3b788d625ddf07203
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-07-31 15:44:06 -0500

    Update ConnectionManagerTest.cs

--- ArkEcosystem.Client.Tests/ConnectionManagerTest.cs
+++ ArkEcosystem.Client.Tests/ConnectionManagerTest.cs
@@ -33,6 +33,7 @@ namespace ArkEcosystem.Client.Tests
         [TestMethod]
         public void Connect()
         {
+            TestHelper.MockHttpRequestTwo("peers"); // dummy request
             var conn = _cm.Connect(TestHelper.MockConnection<Client.API.Two.Two>());
             Assert.AreEqual(conn, _cm.Connection<Client.API.Two.Two>());
             Assert.AreEqual(1, _cm.GetConnections().Count);
@@ -41,6 +42,7 @@ namespace ArkEcosystem.Client.Tests
         [TestMethod]
         public void Disconnect()
         {
+            TestHelper.MockHttpRequestTwo("peers"); // dummy request
             _cm.Connect(TestHelper.MockConnection<Client.API.Two.Two>(), "test");
             Assert.AreEqual(1, _cm.GetConnections().Count);
             _cm.Disconnect("test");
@@ -50,6 +52,7 @@ namespace ArkEcosystem.Client.Tests
         [TestMethod]
         public void Connection()
         {
+            TestHelper.MockHttpRequestTwo("peers"); // dummy request
             var conn = _cm.Connect(TestHelper.MockConnection<Client.API.Two.Two>(), "test");
             Assert.AreEqual(conn, _cm.Connection<Client.API.Two.Two>("test"));
             Assert.AreEqual(1, _cm.GetConnections().Count);
@@ -71,7 +74,9 @@ namespace ArkEcosystem.Client.Tests
         [TestMethod]
         public void GetConnections()
         {
+            TestHelper.MockHttpRequestOne("peers"); // dummy request
             var conn1 = _cm.Connect(TestHelper.MockConnection<Client.API.One.One>(), "test1");
+            TestHelper.MockHttpRequestTwo("peers"); // dummy request
             var conn2 = _cm.Connect(TestHelper.MockConnection<Client.API.Two.Two>(), "test2");
             var dict = _cm.GetConnections();
             Assert.AreEqual(conn1, dict["test1"]);
